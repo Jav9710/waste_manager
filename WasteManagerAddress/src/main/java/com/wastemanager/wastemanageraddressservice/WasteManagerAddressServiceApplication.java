a23101ba2d017d45482dd6f55260f5f805cb0213
Jav9710 <jamv971003@gmail.com>
2024-04-13 12:42:34 -0400

    Escaneo de entidades desde address service sobre common

--- WasteManagerAddress/src/main/java/com/wastemanager/wastemanageraddressservice/WasteManagerAddressServiceApplication.java
+++ WasteManagerAddress/src/main/java/com/wastemanager/wastemanageraddressservice/WasteManagerAddressServiceApplication.java
@@ -2,8 +2,12 @@ package com.wastemanager.wastemanageraddressservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EntityScan("com.wastemanager.model")
 public class WasteManagerAddressServiceApplication {
 
     public static void main(String[] args) {
