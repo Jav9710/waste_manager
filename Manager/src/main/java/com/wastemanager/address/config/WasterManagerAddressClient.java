e6789d5fd0151f3beaa66e2966189d26874ca1cc
Jav9710 <jamv971003@gmail.com>
2024-04-16 17:30:10 -0400

    Service name para feign

--- Manager/src/main/java/com/wastemanager/address/config/WasterManagerAddressClient.java
+++ Manager/src/main/java/com/wastemanager/address/config/WasterManagerAddressClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="${address.name}", url = "#{eurekaClient.getNextServerFromEureka('${discovery.gateway}', false).getHomePageUrl()}${address.name}")
+@FeignClient(name="${address.name}")
 public interface WasterManagerAddressClient {
 
     @GetMapping("/v1/api/{id}")
